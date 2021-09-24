a3c9855ee40fbc557dd5614fbd5944307e17ac60
Felipe Chernicharo <string7dev@gmail.com>
2021-09-24 17:57:23 -0400

    boilerplate ok

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,26 +1,17 @@
-import React, { useState } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import logo from './assets/logo.svg';
 import styles from './App.module.css';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Welcome from './components/Welcome/Welcome';
 
 function App(): JSX.Element {
-  const [count, setCount] = useState<number>(0);
-
   return (
     <Router>
       <div className={styles.App}>
         <header className={styles['App-header']}>
           <img src={logo} className={styles['App-logo']} alt="logo" />
           <Welcome />
-          <p>
-            <button onClick={() => setCount((count) => count + 1)}>
-              count is: {count}
-            </button>
-          </p>
-          <p>
-            Edit <code>App.tsx</code> and save to test HMR updates.
-          </p>
+
           <p>
             <a
               className={styles['App-link']}
@@ -41,6 +32,12 @@ function App(): JSX.Element {
             </a>
           </p>
           <Switch>
+            <Route path="/projects">
+              <main>Projects</main>
+            </Route>
+            <Route path="/contact">
+              <main>Contact</main>
+            </Route>
             <Route path="/about">
               <main>About</main>
             </Route>
--- src/lib/router.ts
+++ src/lib/router.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get('/hello', async (_req, res) => {
-  res.status(200).json({ message: 'Hello World!' });
+  res.status(200).json({ message: 'Hello Melo Dev!' });
 });
 
 export default router;
