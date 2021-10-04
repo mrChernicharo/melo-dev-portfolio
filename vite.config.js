bd5ab81b27b4ca6222a55db724ede04d26b8592f
Felipe Chernicharo <string7dev@gmail.com>
2021-10-03 23:56:11 -0400

    aws deploy prep 2

--- vite.config.js
+++ vite.config.js
@@ -9,15 +9,15 @@ const { PORT = 3001 } = process.env;
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
-  server: {
-    proxy: {
-      '/api': {
-        target: `http://localhost:${PORT}`,
-        changeOrigin: true,
-      },
-    },
-  },
+  // server: {
+  //   proxy: {
+  //     '/api': {
+  //       target: `http://localhost:${PORT}`,
+  //       changeOrigin: true,
+  //     },
+  //   },
+  // },
   build: {
-    outDir: 'dist/app',
+    outDir: 'dist',
   },
 });
